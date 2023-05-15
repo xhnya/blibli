85aecf0ceaf23a851ecea4c514d573f1f62d0245
xhn930957853 <930957853@qq.com>
2023-05-15 10:13:56 +0800

    添加用户模块

---
+++ hilihili-user/src/main/java/com/xhn/hilihili/user/HilihiliUserApplication.java
@@ -0,0 +1,13 @@
+package com.xhn.hilihili.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HilihiliUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HilihiliUserApplication.class, args);
+    }
+
+}
---
+++ hilihili-user/src/test/java/com/xhn/hilihili/user/HilihiliUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhn.hilihili.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HilihiliUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
