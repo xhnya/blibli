7c93f5ded44be4c8a60d61d4b237acfa46e9e402
xhn930957853 <930957853@qq.com>
2023-05-15 10:11:52 +0800

    添加认证中心服务

---
+++ hilihili-auth/src/main/java/com/xhn/hilihili/auth/HilihiliAuthApplication.java
@@ -0,0 +1,13 @@
+package com.xhn.hilihili.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HilihiliAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HilihiliAuthApplication.class, args);
+    }
+
+}
---
+++ hilihili-auth/src/test/java/com/xhn/hilihili/auth/HilihiliAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhn.hilihili.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HilihiliAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
